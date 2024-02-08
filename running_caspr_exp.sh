90f360edd02f29106b66b73217e59e0c4f7298c8
dvsaisurya <saisurya@utexas.edu>
2024-02-08 18:58:50 +0000

    made alpha 1.0 in caspradaptive

--- running_caspr_exp.sh
+++ running_caspr_exp.sh
@@ -1,7 +1,7 @@
 
 #used a b2 of 0.8 for lambdas of caspr adaptive but interpolation to casprhm afterwards with adagrad update
 EXP_DIR="/home/saisurya/Projects/algorithmic-efficiency/eff_caspr_hm_adaptive"
-EXP_NAME="eff_caspr_hm_adaptive_corrected_adagrad_lambeps_1e-2_b2_0.8_interpolation_upto_10k_2_all_hparams"
+EXP_NAME="eff_caspr_hm_adaptive_corrected_adagrad_lambeps_1e-2_b2_0.8_interpolation_upto_10k_8_all_hparams"
 SUBMISSION_PATH="prize_qualification_baselines/external_tuning/efficient_caspr_adaptive_target_setting.py"
 SEARCH_SPACE_PATH="prize_qualification_baselines/external_tuning/tuning_search_space.json"
 WORKLOAD="ogbg"
