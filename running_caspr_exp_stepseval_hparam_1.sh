62ca0b5a549d80c88d4830c8f740103a950ca0be
dvsaisurya <saisurya@utexas.edu>
2024-02-15 23:46:45 +0000

    added steps eval wmt

--- running_caspr_exp_stepseval_hparam_1.sh
+++ running_caspr_exp_stepseval_hparam_1.sh
@@ -1,11 +1,11 @@
 
 #used b3 of 0.8 which is fixed throughout the training and runs caspr_adaptive with hparam_1 for 5 times with different seeds.
 EXP_DIR="/home/saisurya/Projects/algorithmic-efficiency/eff_caspr_hm_adaptive"
-EXP_NAME="eff_caspr_hm_adaptive_corrected_adagrad_lambeps_1e-2_b2_0.8_fixed_with_clipping_with_stepseval_5_hparam_1"
-SUBMISSION_PATH="prize_qualification_baselines/external_tuning/efficient_caspr_adaptive_target_setting.py"
+EXP_NAME="eff_caspr_hm_adaptive_corrected_adagrad_lambeps_1e-2_b2_0.8_fixed_with_clipping_with_stepseval_7_hparam_1"
+SUBMISSION_PATH="prize_qualification_baselines/external_tuning/efficient_caspr_adaptive_dist_inv_target_setting.py"
 SEARCH_SPACE_PATH="prize_qualification_baselines/external_tuning/tuning_search_space_hparam_1.json"
-WORKLOAD="ogbg"
-DATA_DIR="~/data"
+WORKLOAD="wmt"
+DATA_DIR="~/data/wmt"
 RNG_SEED="2"
 python3 submission_runner.py \
     --framework=jax \
