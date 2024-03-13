1a10818da4ce2a99f88fcf883ebbdfe22e3af06a
dvsaisurya <saisurya@utexas.edu>
2024-03-13 01:26:46 +0000

    setup imagenet hparam search

--- running_caspr_exp_stepseval_hparam_1_imagenet.sh
+++ running_caspr_exp_stepseval_hparam_1_imagenet.sh
@@ -2,7 +2,7 @@
 #used b3 of 0.8 which is fixed throughout the training and runs caspr_adaptive with hparam_1 for 5 times with different seeds.
 # EXP_DIR="/home/saisurya/Projects/algorithmic-efficiency/eff_caspr_hm_adaptive"
 EXP_DIR="/home/saisurya/Projects/algorithmic-efficiency/eff_caspr_hm_shampoo"
-EXP_NAME="eff_caspr_hm_shampoo_adagrad_all_hparams_1"
+EXP_NAME="eff_caspr_hm_shampoo_adagrad_all_hparams_2"
 # SUBMISSION_PATH="prize_qualification_baselines/external_tuning/efficient_caspr_adaptive_fast_dist_inv_target_setting.py"
 # SUBMISSION_PATH="prize_qualification_baselines/external_tuning/efficient_caspr_adaptive_dist_inv_target_setting.py"
 # SUBMISSION_PATH="prize_qualification_baselines/external_tuning/jax_nadamw_target_setting.py"
