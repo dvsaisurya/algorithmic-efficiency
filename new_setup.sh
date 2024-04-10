9d6acb95c26221e86d44b87732b7cb955cabfe9e
saisurya <saisurya@utexas.edu>
2024-04-10 01:09:55 +0000

    new_setup file changed

--- new_setup.sh
+++ new_setup.sh
@@ -1,3 +1,5 @@
+python3 -m venv env
+source env/bin/activate  
 pip install -r requirements.txt
 pip uninstall jax jaxlib --yes
 pip install jax[tpu]==0.4.10 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
