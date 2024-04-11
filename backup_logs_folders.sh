d84b57e946b2c574408f7cd88ed8cb8c7212d21b
saisurya <saisurya@utexas.edu>
2024-04-11 18:42:36 +0000

    added backup code

---
+++ backup_logs_folders.sh
@@ -0,0 +1,5 @@
+cp -r logs logs-3tpuv3
+gsutil -m cp -r logs-3tpuv3 gs://casprexpleafy/
+
+cp -r eff_caspr_hm_adaptive_submission eff_caspr_hm_adaptive_submission-3tpuv3
+gsutil -m cp -r eff_caspr_hm_adaptive_submission-3tpuv3 gs://casprexpleafy/
