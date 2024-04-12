d3d39fd4e72ec21e244ef38e41afcf95743288dc
dvsaisurya <saisurya@utexas.edu>
2024-04-12 00:07:38 +0000

    added json files and bug fixes to sh files

--- wmt_search_adamw_0.375.sh
+++ wmt_search_adamw_0.375.sh
@@ -9,7 +9,7 @@ source env/bin/activate
 #used b3 of 0.8 which is fixed throughout the training and runs caspr_adaptive with hparam_1 for 5 times with different seeds.
 EXP_DIR="/home/saisurya/Projects/algorithmic-efficiency/eff_caspr_hm_adaptive_submission"
 EXP_NAME="eff_submission_adamw_wmt_search_$1"
-SUBMISSION_PATH="submission_folder/external_tuning/caspr_adaptive/jax_adamw_target_setting_0.375.py"
+SUBMISSION_PATH="submission_folder/external_tuning/caspr_adaptive/jax_adamw_target_setting_0375.py"
 SEARCH_SPACE_PATH="adamw_0.375_jsons/tuning_search_space_adamw_0.375_$1.json"
 WORKLOAD="wmt"
 DATA_DIR="~/data/wmt"
